6844761e5f127a3460fc7afe4130c1bdbb9aba77
CoolMb39 <97110658+CoolMb39@users.noreply.github.com>
2022-07-12 19:55:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-wIqACptABEwW4Q01EdXXp081Sk_7eLw",
+    authDomain: "test-project-31d12.firebaseapp.com",
+    projectId: "test-project-31d12",
+    storageBucket: "test-project-31d12.appspot.com",
+    messagingSenderId: "1009589254830",
+    appId: "1:1009589254830:web:1dfcb851ce8eff09d6cb3f"
+  };
